51af50483a6312c6c4ce0f4d8812b45b5837031e
Paul Sinnett <paul.sinnett@gmail.com>
2020-12-01 12:13:49 +0000

    Initial commit

---
+++ Assets/ItemDatabase.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Item
+{
+    public string name;
+    public int price;
+}
+
+[CreateAssetMenu]
+public class ItemDatabase : ScriptableObject
+{
+    public List<Item> items;
+}
---
+++ Assets/Shop.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shop : MonoBehaviour
+{
+    public ItemDatabase database;
+
+    void Start()
+    {
+        foreach (Item item in database.items)
+        {
+            Debug.LogFormat("{0} {1} coins", item.name, item.price);
+        }    
+    }
+}
